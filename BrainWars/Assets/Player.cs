64e1599e4dcd2112e3057ce34b70b46de690458e
omegamotor <40634974+omegamotor@users.noreply.github.com>
2020-04-01 15:44:29 +0200

    BrainWars v0.01

---
+++ BrainWars/Assets/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public int fullShield = 100;
+    public int currentShieldStatus;
+    public ShieldBarScript shieldbar;
+
+    // Start is called before the first frame update
+    
+    
+    void Start()
+    {
+        currentShieldStatus = fullShield;
+        shieldbar.SetCooldownTime(fullShield);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UseShield();
+        }
+
+
+
+    }
+
+    void UseShield()
+    {
+        if(shieldbar.Ready())
+        {
+            currentShieldStatus = 0;
+            shieldbar.SetShield(currentShieldStatus);
+        }
+        
+
+    }
+
+
+
+
+}
---
+++ BrainWars/Assets/ShieldBarScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ShieldBarScript : MonoBehaviour
+{
+
+    public Slider slider;
+
+    public void SetCooldownTime(int time)
+    {
+        slider.maxValue = time;
+        slider.value = time;
+    }
+
+    public void SetShield(int shieldCooldown)
+    {
+        slider.value = shieldCooldown;
+        if (shieldCooldown == 0) InvokeRepeating("ResetShield", 0, 1.0f);
+
+    }
+
+    public void ResetShield()
+    {
+        slider.value += 10;
+        if (slider.value == slider.maxValue) CancelInvoke("ResetShield");
+
+    }
+
+    public bool Ready()
+    {
+        if (slider.value == slider.maxValue) return true;
+        
+        else return false;          
+    }
+    
+}
