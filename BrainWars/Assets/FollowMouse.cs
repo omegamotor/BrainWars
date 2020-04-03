4fd6b08691f5f7f610863dc9bbc07e9f9c26d9df
omegamotor <40634974+omegamotor@users.noreply.github.com>
2020-04-03 02:18:53 +0200

    Jednostki
    rozkaz poruszenia się w danym kierunku
    + Licznik Spawnu kolejnej jednostki w bazie co x sekund (memory 1)
    + Kolizja z pamięcią

---
+++ BrainWars/Assets/FollowMouse.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    Vector3 mousePosition;
+    public float moveSpeed = 0.001f;
+    Rigidbody2D rb;
+    Vector2 position;
+    bool start = false;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        Run();
+    }
+
+
+    void Run()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+            rb.MovePosition(position);
+            //Dodałem start bo taleportowało na środek planszy po włączeniu
+            start = true;
+        }
+
+        else if (start)
+        {
+            position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+            rb.MovePosition(position);
+        }
+    }
+
+
+}
---
+++ BrainWars/Assets/Warrior.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warrior : MonoBehaviour
+{
+
+    public Transform warrior;
+
+    private Vector3 mousePos;
+    private Rigidbody2D rb;
+    private Vector2 direction;
+    private float moveSpeed = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+
+
+            
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            Vector2 warrior2D = new Vector2(warrior.position.x, warrior.position.y);
+
+            //Przenieś przedmiot w miejsce gdzie wcisnołem
+            //warrior.position = new Vector3(mousePos2D[0], mousePos2D[1], 0);
+
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////
+            //    mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //    direction = (mousePos - transform.position).normalized;
+            //    rb.velocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);
+
+            //}
+
+         //else
+         //{
+         //    rb.velocity = Vector2.zero;
+         //
+         /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        }
+
+
+
+        
+    }
+}
